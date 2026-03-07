e46ff7495c1ebb317720beb0d542f81ab44dc579
FatTran <tranphat1512@gmail.com>
2026-03-07 15:40:17 +0700

    Create install script for hyprland

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dotfiles
 A configuration for Hyprland on Manjaro
+
+#### To install Hyprland ####
+
+Run the install.sh script in this repository, this script will install some of the dependencies for this configuration
+
+Standing from this directory, use stow to import the necessary configuration
+
+Example: stow nvim 
+
+
+
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pacman -Syu cava nvim rofi swaybg swaync waybar waypaper wezterm wlogout nwg-look xdg-desktop-portal-hyprland qt5-wayland qt6-wayland ttf-cascadia-code-nerd chromium fcitx5 fcitx5-bamboo fcitx5-configtool fcitx5-qt fcitx5-gtk fcitx5-configtool hyprland network-manager-applet
+
+yay -S waypaper wlogout catppuccin-gtk-theme-mocha vtop
+
+
